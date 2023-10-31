6954afd3684319098d84234b32212409e0338488
khanhninhnguyen <khanhninhsp@gmail.com>
2023-10-31 11:59:40 +0100

    fix error of na value when fit IWLS

--- attribution.R
+++ attribution.R
@@ -26,7 +26,6 @@ source(file = paste0(path_code, "support_data_characterization.R"))
 #   # 
 # }
 
-
 # SELECT NEARBY STATIONS --------------------------------------------------
 #' 3 tests: 
 #' * limit by other brps 
@@ -39,9 +38,9 @@ list_brp = extract_list_brp(date_mean)
 list_brp <- list_brp %>%
   group_by(name) %>%
   mutate(cluster_index = get_clusters(brp, 80)) 
-infor_all = extract_info_nearby(path_data = path_data_NGL,
-                                list_brp = list_brp,
-                                path_results = path_results)
+# infor_all = extract_info_nearby(path_data = path_data_NGL,
+#                                 list_brp = list_brp,
+#                                 path_results = path_results)
 column_classes <- c("character", "Date", "character", rep("numeric",3),
                     rep("Date", 4), rep("numeric", 12))
 infor_all = read.table(file = paste0(path_results, "pre_info_test.txt"), 
@@ -60,14 +59,23 @@ infor_all <- infor_all %>%
 
 # check selection conditions
 check_selected(list_brp = list_brp, infor_all = infor_all,
-               nbcsv_min = 250, distance = 200, rate_consecutive = 0.9)
-
-df_long <- infor_all[,c(11:16)] %>%
-  gather(key = "variable", value = "value")
+               nbcsv_min = 200, distance = 200, rate_consecutive = 0.)
 
 # Plot histograms for each column
+nbcsv_min = 200
+infor_sel <- infor_all %>% 
+  filter(noise < 2, 
+         n_main_bef > nbcsv_min, 
+         n_nearby_bef > nbcsv_min, 
+         n_joint_bef > nbcsv_min, 
+         n_main_aft > nbcsv_min, 
+         n_nearby_aft > nbcsv_min, 
+         n_joint_aft > nbcsv_min)
+df_long <- infor_all[,c(17:22)] %>%
+  gather(key = "variable", value = "value")
+
 ggplot(df_long, aes(x = value)) +
-  geom_histogram(binwidth = 200, fill = "blue", color = "black", alpha = 0.7) +
+  geom_histogram(binwidth = 0.05, fill = "blue", color = "black", alpha = 0.7) +
   facet_wrap(~ variable, scales = "free_y") +
   theme_minimal() +
   labs(title = "Histograms of Multiple Columns", x = "Value", y = "Frequency")
@@ -75,7 +83,7 @@ ggplot(df_long, aes(x = value)) +
 # CHARACTERIZATION --------------------------------------------------------
 # includes 2 steps: 1.fit IGLS to get WLS residual 
 # 2. fit normalized residual to ARMA models 
-nbcsv_min = 250 
+nbcsv_min = 200 
 infor_selected <- infor_all %>%
   filter(noise < 2, 
          n_main_bef > nbcsv_min, 
@@ -123,17 +131,27 @@ for (i in c(1:nrow(infor_selected))) {
   for (j in c(1:6)) {
     name.series0 = name_six_diff[j]
     m = construct_design(df_data, name.series = name.series0, break.ind = brp_ind)
+    nna_ind = which(!is.na(m$signal))
+    
+    norm_res <- rep(NA, nrow(m))
+    fit_igls_var <- rep(NA, nrow(m))
+    fit_igls_res <- rep(NA, nrow(m))
+    
     tol0 = 0.01
     if(i == 49 & j ==5){ tol0 = 0.0001 }
-    fit.igls = IGLS(design.m = m, tol =  tol0, day.list = df_data$Date)
-    norm_res = unlist(fit.igls$residual)/sqrt(unlist(fit.igls$var))
+    fit_igls = IGLS(design.m = m[nna_ind,], tol =  tol0, day.list = df_data$Date[nna_ind])
+    norm_res[nna_ind] = unlist(fit_igls$residual)/sqrt(unlist(fit_igls$var))
     arima_fit = fit.arima(norm_res)
     
-    Res_IWLS[, paste0(name.series0, '_var')] <- unlist(fit.igls$var)
-    Res_IWLS[, paste0(name.series0, '_res')] <- unlist(fit.igls$residual)
+    fit_igls_var[nna_ind] <- unlist(fit_igls$var)
+    fit_igls_res[nna_ind] <- unlist(fit_igls$residual)
+    
+    Res_IWLS[, paste0(name.series0, '_var')] <- fit_igls_var
+    Res_IWLS[, paste0(name.series0, '_res')] <- fit_igls_res
+    
     ARMA_order[i,c((3*j-2):(3*j))] = arima_fit$pq
     ARMA_coef[i,c((4*j-3):(4*j))] = arima_fit$coef
-    Four_coef[i,c((10*j-9):(10*j))] = fit.igls$coefficients
+    Four_coef[i,c((10*j-9):(10*j))] = fit_igls$coefficients
   }
   print(i)
 }
