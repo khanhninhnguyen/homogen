4fd10ccc2bec55fccc55386e6622d4e764b7341a
khanhninhnguyen <khanhninhsp@gmail.com>
2023-11-08 21:04:12 +0100

    test

--- verify.R
+++ verify.R
@@ -6,3 +6,20 @@ infor_all = read.table(file = paste0(path_results, "pre_info_test.txt"),
                        header = TRUE, colClasses = column_classes)
 rpt_data <- read.table(file = paste0(path_data, "support/liste_main20yr_1nearby_200km_500m_np250_nd250.rpt"),
                        header = TRUE, check.names = FALSE) 
+rpt_data$t_break = as.Date(rpt_data$t_break)
+
+nbcsv_min = 249
+infor_sel <- infor_all %>% 
+  filter(noise < 2, 
+         n_main_bef > nbcsv_min, 
+         n_nearby_bef > nbcsv_min, 
+         n_joint_bef > nbcsv_min, 
+         n_main_aft > nbcsv_min, 
+         n_nearby_aft > nbcsv_min, 
+         n_joint_aft > nbcsv_min)
+
+test <- infor_sel %>% 
+  left_join(rpt_data, 
+            by = join_by(main == name_main, 
+                         brp == t_break,
+                         nearby == name_nearby))
