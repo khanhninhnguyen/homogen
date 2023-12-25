5b2385b2a7f0bfc70e0262df5a22194836a173cf
khanhninhnguyen <khanhninhsp@gmail.com>
2023-12-25 23:14:43 +0100

    test

--- attribution_c.R
+++ attribution_c.R
@@ -164,7 +164,7 @@ column_classes <- c(rep("character",2), rep("numeric",3),
 list_characteried_segments = read.table(file = paste0(path_results, 
                                                   "List_longest_segment.txt"), 
                                     header = TRUE, colClasses = column_classes)
-for (i in c(1:2000)) {
+for (i in c(2001:4000)) {
   fit.i = list()
   
   main_st = selected_cases$main[i]
